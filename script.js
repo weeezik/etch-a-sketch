17d4e52accdcf31b933376723b0c3fc0f162eba2
weeezik <mkihanya.33@gmail.com>
2024-03-04 15:12:37 +0000

    added style sheet
    creating html elements with javascript. formatting those elements via the styles.css file

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //container flex box and flex-wrap set to wrap
 const container = document.querySelector(".container");
-container.setAttribute("style", "display: flex; flex-wrap: wrap");
+
+
 
 
 let squaresPerSide = 2;
@@ -8,10 +9,9 @@ let totalSquares = squaresPerSide*squaresPerSide;
 
 function createDiv () {
     let newDiv = document.createElement("div");
-    container.appendChild(newDiv);  
-    newDiv.setAttribute("style", "background-color: red; width: 200px; height: 200px");
+    container.appendChild(newDiv);
+    newDiv.classList.add("box");
 }
-
 
 createDiv();
 
