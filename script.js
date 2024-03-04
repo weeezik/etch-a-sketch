8d92032dd4439d81ab0b0c8ed8870cd9c408e369
weeezik <mkihanya.33@gmail.com>
2024-03-04 17:22:10 +0000

    attempts at row by row method
    nothing going right now. just wanted to commit before i take a break.
    attempt: create a row. fill that box with squaresPerSide number of divs. then create a new row that has the same amount of divs.
    then keep doing that until the amount of rows equals squaresPerSide. essentially we need to create squaresPerSide rows and squaresPerSide div's with each of those rows.
    gratitude. energy. learning. embrace the suck.

--- script.js
+++ script.js
@@ -3,15 +3,33 @@ const container = document.querySelector(".container");
 let squaresPerSide = 2;
 let totalSquares = squaresPerSide*squaresPerSide;
 
+
+
+
 function createDiv () {
     let newDiv = document.createElement("div");
-    container.appendChild(newDiv);
+    newRow.appendChild(newDiv);
     newDiv.classList.add("box");
 }
 
-let i = 0;
-do {
-    createDiv();
-    i++;
-} while (i < totalSquares);
+function createRow () {
+    
+    let newRow = document.createElement("div");
+    container.appendChild(newRow);
+    newRow.classList.add("row");
+    
+}
+
+
+// let i = 0;
+// do {
+//     createRow();
+//     createDiv();
+//     i++;
+// } while (i < totalSquares);
+
+// for (let i = 0; i = squaresPerSide; i++) {
+//     createDiv();
+// }
+
 
