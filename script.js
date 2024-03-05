3f7394483a4529bf51956ae21f525d820943ecc7
weeezik <mkihanya.33@gmail.com>
2024-03-05 16:57:44 +0000

    problems with grid set up here
    need to delete the previously made div's when the user input grid is made. then again when a new user input grid is made etc etc

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ buttonDiv.append(button);
 
 
 //squaresPerSide is the value X in a X by X grid that we are making 
-let squaresPerSide = 16;
 
+let squaresPerSide = 5;
 for (let i = 1; i <= squaresPerSide; i++) {
     //create a row div
     const row = document.createElement("div");
@@ -25,31 +25,30 @@ for (let i = 1; i <= squaresPerSide; i++) {
     }
 }
 
-function createGrid () {
+button.addEventListener("click", () => {
+    let squaresPerSide = Number(prompt("Enter your X value. This grid is built in a X by X format."));
+    //if number of clicks is less than 1 do not run remove, else do run remove (i.e., run remove when iterations is > 1)
     for (let i = 1; i <= squaresPerSide; i++) {
+        // row.remove();
+
         //create a row div
         const row = document.createElement("div");
         row.classList.add("row");
         container.append(row);
         for (let j = 1; j <= squaresPerSide; j++) {
+            // square.remove();
+
             //create a square div
             const square = document.createElement("div");
             square.classList.add("square");
             row.append(square);
         }
     }
-}
-
-button.addEventListener("click", () => {
-    let userSquares = prompt("Enter your X value. This grid is built in a X by X format.")
-    let squaresPerSide = Number(userSquares);
-    if (squaresPerSide > 100) {
-        prompt("ERROR. Keep number below 100");
-    } else {
-        return squaresPerSide;
-    }
-    createGrid();
 });
+
+
+
+
 
 
 
