08b090997a0a1839c33b19bc363293e7dc824ac1
weeezik <mkihanya.33@gmail.com>
2024-03-05 16:16:17 +0000

    looking good so far. button logic needs fixing
    fix the button logic to rerun the createGrid function with the entered grid value.
    maybe use a console log in there to keep track of the squaresPerSide value.

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 //set container item to a variable
 const container = document.querySelector(".container");
 
+//add button
+const buttonDiv = document.createElement("div");
+document.body.append(buttonDiv);
+const button = document.createElement("button");
+button.textContent = "Click here to set grid size.";
+buttonDiv.append(button);
+
+
 //squaresPerSide is the value X in a X by X grid that we are making 
 let squaresPerSide = 16;
 
@@ -17,11 +25,31 @@ for (let i = 1; i <= squaresPerSide; i++) {
     }
 }
 
+function createGrid () {
+    for (let i = 1; i <= squaresPerSide; i++) {
+        //create a row div
+        const row = document.createElement("div");
+        row.classList.add("row");
+        container.append(row);
+        for (let j = 1; j <= squaresPerSide; j++) {
+            //create a square div
+            const square = document.createElement("div");
+            square.classList.add("square");
+            row.append(square);
+        }
+    }
+}
 
-
-
-//hover event listener, for elements with class="square"
-//within event listener there is a function that changes the color of the div to plum (hover color = plum)
+button.addEventListener("click", () => {
+    let userSquares = prompt("Enter your X value. This grid is built in a X by X format.")
+    let squaresPerSide = Number(userSquares);
+    if (squaresPerSide > 100) {
+        prompt("ERROR. Keep number below 100");
+    } else {
+        return squaresPerSide;
+    }
+    createGrid();
+});
 
 
 
