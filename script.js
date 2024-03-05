c24d4c9b32fbb5cb662318f30ae96068a04d5b68
weeezik <mkihanya.33@gmail.com>
2024-03-05 17:20:42 +0000

    still not removing previous grid...
    before creating a new grid, so grids are just stacking up on top of each other. I need a way to create a discrete naming system or iterate in a way such that the previous grid is removable before the next grid is made.

--- script.js
+++ script.js
@@ -11,13 +11,13 @@ buttonDiv.append(button);
 
 //squaresPerSide is the value X in a X by X grid that we are making 
 
-let squaresPerSide = 5;
-for (let i = 1; i <= squaresPerSide; i++) {
+// let squaresPerSide = 5;
+for (let i = 1; i <= 5; i++) {
     //create a row div
     const row = document.createElement("div");
     row.classList.add("row");
     container.append(row);
-    for (let j = 1; j <= squaresPerSide; j++) {
+    for (let j = 1; j <= 5; j++) {
         //create a square div
         const square = document.createElement("div");
         square.classList.add("square");
@@ -25,26 +25,31 @@ for (let i = 1; i <= squaresPerSide; i++) {
     }
 }
 
-button.addEventListener("click", () => {
-    let squaresPerSide = Number(prompt("Enter your X value. This grid is built in a X by X format."));
-    //if number of clicks is less than 1 do not run remove, else do run remove (i.e., run remove when iterations is > 1)
-    for (let i = 1; i <= squaresPerSide; i++) {
-        // row.remove();
-
-        //create a row div
-        const row = document.createElement("div");
-        row.classList.add("row");
-        container.append(row);
-        for (let j = 1; j <= squaresPerSide; j++) {
-            // square.remove();
-
-            //create a square div
-            const square = document.createElement("div");
-            square.classList.add("square");
-            row.append(square);
+function userGrid () {
+    button.addEventListener("click", () => {
+        let squaresPerSide = Number(prompt("Enter your X value. This grid is built in a X by X format."));
+        //if number of clicks is less than 1 do not run remove, else do run remove (i.e., run remove when iterations is > 1)
+        for (let i = 1; i <= squaresPerSide; i++) {
+            //create a row div
+            const row = document.createElement("div");
+            row.classList.add("row");
+            container.append(row);
+            for (let j = 1; j <= squaresPerSide; j++) {
+                //create a square div
+                const square = document.createElement("div");
+                square.classList.add("square");
+                row.append(square);
+            }
         }
-    }
-});
+    });
+};
+
+
+userGrid();
+// container.remove(document.querySelector(".row"));
+
+
+
 
 
 
