9c558c881bc23992622d1328136e56e906e85af0
weeezik <mkihanya.33@gmail.com>
2024-03-07 16:29:56 +0000

    new structure to assigning div elements an id.
    maybe change to have the id's just be on the rows. because those will be the only thing i delete to add the new grid. use id variable and console.log's to track their value/debug the logic

--- script.js
+++ script.js
@@ -9,56 +9,47 @@ button.textContent = "Click here to set grid size.";
 buttonDiv.append(button);
 
 //default settings with squaresPerSide === 5
-let squaresPerSide = 5;
-for (let i = 1; i <= 5; i++) {
-    //create a row div
-    const row = document.createElement("div");
-    row.classList.add("row");
-    container.append(row);
-    for (let j = 1; j <= 5; j++) {
-        //create a square div
-        const square = document.createElement("div");
-        square.classList.add("square");
-        row.append(square);
-    }
-}
+// let squaresPerSide = 5;
+// for (let i = 1; i <= 5; i++) {
+//     //create a row div
+//     const row = document.createElement("div");
+//     row.classList.add("row");
+//     container.append(row);
+//     for (let j = 1; j <= 5; j++) {
+//         //create a square div
+//         const square = document.createElement("div");
+//         square.classList.add("square");
+//         row.append(square);
+//     }
+// }
 
 function createGrid () {
     //remove elements with the id count-1 (i.e., the previous count)
-    let count = 0;
     let squaresPerSide = Number(prompt("Enter your X value. This grid is built in a X by X format."));
     for (let i = 1; i <= squaresPerSide; i++) {        
         //create a row div
         const currentRow = document.createElement("div");
         currentRow.classList.add("row");
-        //add id = i to row
-        currentRow.setAttribute("id", count);
-        //DOM manipulation here
+        currentRow.setAttribute("id", id);
         container.append(currentRow);
         for (let j = 1; j <= squaresPerSide; j++) {
-            //create a square div
             const currentSquare = document.createElement("div");
             currentSquare.classList.add("square");
-            //add id = i to currentSquare elements
-            currentSquare.setAttribute("id", count);
-            //DOM manipulation here
+            currentSquare.setAttribute("id", id);
             currentRow.append(currentSquare);
         }
+        
     }
-    count++;
 }
 
 
+let id = 1;
+button.addEventListener("click", (id) => {    
+    createGrid(id);
+    id++;
+});
 
-button.addEventListener("click", () => {
-    createGrid();   
-})
-
-
-//remove previous row
-    // document.removeChild(container.firstElementChild)
-
-    //remove previous square divs
+// document.removeChild(container.firstElementChild)
 
 
 
