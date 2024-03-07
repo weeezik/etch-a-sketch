64f550110a58155d2f8899ca70ef0b6a834608ce
weeezik <mkihanya.33@gmail.com>
2024-03-07 15:08:54 +0000

    first version of the button debug branch
    still having issues with square divs filling the container with
    the previous square divs. delete the previous divs then
    create and append the current input divs

--- script.js
+++ script.js
@@ -8,42 +8,39 @@ const button = document.createElement("button");
 button.textContent = "Click here to set grid size.";
 buttonDiv.append(button);
 
-// let squaresPerSide = 5;
-for (let i = 1; i <= 5; i++) {
-    //create a row div
-    const row = document.createElement("div");
-    row.classList.add("row");
-    container.append(row);
-    for (let j = 1; j <= 5; j++) {
-        //create a square div
-        const square = document.createElement("div");
-        square.classList.add("square");
-        row.append(square);
-    }
-}
-
-function userGrid () {
-    button.addEventListener("click", () => {
-        let squaresPerSide = Number(prompt("Enter your X value. This grid is built in a X by X format."));
-        //if number of clicks is less than 1 do not run remove, else do run remove (i.e., run remove when iterations is > 1)
-        for (let i = 1; i <= squaresPerSide; i++) {
-            //create a row div
-            const row = document.createElement("div");
-            row.classList.add("row");
-            container.append(row);
-            for (let j = 1; j <= squaresPerSide; j++) {
-                //create a square div
-                const square = document.createElement("div");
-                square.classList.add("square");
-                row.append(square);
-            }
+button.addEventListener("click", () => {
+    let squaresPerSide = Number(prompt("Enter your X value. This grid is built in a X by X format."));
+    for (let i = 1; i <= squaresPerSide; i++) {
+        //delete
+        
+        //create a row div
+        const row = document.createElement("div");
+        row.classList.add("row");
+        container.append(row);
+        for (let j = 1; j <= squaresPerSide; j++) {
+            //create a square div
+            const square = document.createElement("div");
+            square.classList.add("square");
+            row.append(square);
         }
-    });
-};
+    }
+});
+
+// let squaresPerSide = 5;
+// for (let i = 1; i <= 5; i++) {
+//     //create a row div
+//     const row = document.createElement("div");
+//     row.classList.add("row");
+//     container.append(row);
+//     for (let j = 1; j <= 5; j++) {
+//         //create a square div
+//         const square = document.createElement("div");
+//         square.classList.add("square");
+//         row.append(square);
+//     }
+// }
 
 
-userGrid();
-// container.remove(document.querySelector(".row"));
 
 
 
