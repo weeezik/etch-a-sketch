adf35b883990c73f1727faa9acb17a7256108364
weeezik <mkihanya.33@gmail.com>
2024-03-08 16:22:27 +0000

    cleaning up code, final touches
    deleted a bunch of old comments

--- script.js
+++ script.js
@@ -11,54 +11,38 @@ buttonDiv.append(button);
 const oldContainer = document.createElement("div");
 container.appendChild(oldContainer);
 oldContainer.classList.add("oldContainer");
-// oldContainer.setAttribute("id", "aDiv")
-
-// default settings with squaresPerSide === 5
 
 for (let i = 1; i <= 5; i++) {
-    //create a row div
     const row = document.createElement("div");
     row.classList.add("row");
     oldContainer.append(row);
     for (let j = 1; j <= 5; j++) {
-        //create a square div
         const square = document.createElement("div");
         square.classList.add("square");
         row.append(square);
     }
 }
 
-
-
 function createGrid() {
     const newContainer = document.createElement("div");
-    // newContainer.setAttribute("id", "aDiv");
     container.appendChild(newContainer);
-    //maybe use [i or count in the newContainer to delete newContainer[count-1] while adding newContainer[count]]
     newContainer.classList.add("newContainer");
-    //remove elements with the id count-1 (i.e., the previous count)
     let squaresPerSide = Number(prompt("This grid is built in a X by X format. Enter your X value (max. 100): "));
     for (let i = 1; i <= squaresPerSide; i++) {
-        //create a row div
         const currentRow = document.createElement("div");
         currentRow.classList.add("row");
-        // currentRow.setAttribute("id", squaresPerSide);
         newContainer.append(currentRow);
         for (let j = 1; j <= squaresPerSide; j++) {
             const currentSquare = document.createElement("div");
             currentSquare.classList.add("square");
-            // currentSquare.setAttribute("id", squaresPerSide);
             currentRow.append(currentSquare);
         }
     }
 }
 
 button.addEventListener("click", () => {
-    // oldContainer.remove();
-    // container.removeChild([0]);
     container.childNodes[0].remove();
     createGrid();
-    //use id method to delete previously generated user grids.
 });
 
 
